968de4ba618b7a16ed59d130e3ff86d91bc0907e
Mahendra Dinata <remdinata@gmail.com>
2023-02-17 22:06:16 +0700

    add tailwindcss and configure live reload

--- src/main/java/com/pplbo/fortic5/model/user/Customer.java
+++ src/main/java/com/pplbo/fortic5/model/user/Customer.java
@@ -1,17 +1,36 @@
 package com.pplbo.fortic5.model.user;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 
 @Entity
 @Table(name = "customers")
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Customer extends User {
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private String mobileNumber;
 
     private Date dateOfBirth;
 
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    mode: process.env.NODE_ENV ? 'jit' : undefined,
+    content: ["./src/**/*.html", "./src/**/*.js"],
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/aspect-ratio')
+    ],
+}
