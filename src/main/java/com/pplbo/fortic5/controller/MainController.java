d99a086ac5eb2a417fd94002dafe3557905e5043
Mahendra Dinata <remdinata@gmail.com>
2023-03-10 17:21:49 +0700

    add html template for home page and product detail

--- src/main/java/com/pplbo/fortic5/controller/MainController.java
+++ src/main/java/com/pplbo/fortic5/controller/MainController.java
@@ -10,4 +10,9 @@ public class MainController {
     public String homePage() {
         return "home";
     }
+
+    @GetMapping("/product")
+    public String productDetail() {
+        return "product";
+    }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,34 @@ module.exports = {
     mode: process.env.NODE_ENV ? 'jit' : undefined,
     content: ["./src/**/*.html", "./src/**/*.js"],
     theme: {
-        extend: {},
+        colors: {
+            transparent: 'transparent',
+            'cpt11': '#EDF1D6',
+            'cpt12': '#9DC08B',
+            'cpt13': '#609966',
+            'cpt14': '#40513B',
+            'cpt21': '#3C6255',
+            'cpt22': '#61876E',
+            'cpt23': '#A6BB8D',
+            'cpt24': '#EAE7B1',
+            'white': '#FFFFFF',
+            'black': '#0B0E0A',
+        },
+        fontFamily: {
+            'primary' : 'Montserrat, sans-serif',
+            'secondary': 'Nunito, sans-serif',
+        },
+        fontWeight: {
+            normal: '400',
+            medium: '500',
+            bold: '700',
+            extra: '900',
+        },
+        extend: {
+            gridTemplateColumns: {
+                'product': 'repeat(auto-fill, minmax(70px, 1fr)',
+            },
+        },
     },
     variants: {
         extend: {},
